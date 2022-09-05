454ea883518b9daf819c60a7dfdf4a98807ae80b
state-dogs-001 <darkness.state.001@gmail.com>
2022-09-05 17:35:32 +0700

    update 17.35 09-05-22

--- app/Http/Controllers/Api/EquipmentController.php
+++ app/Http/Controllers/Api/EquipmentController.php
@@ -5,7 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Equipment;
+use App\Models\EquipmentStatus;
+
 class EquipmentController extends Controller
 {
-    //
+    //? Index (Paginate)
+
+    //? Show For Read
+
+    //? Show For Update
+
+    //? Search (Paginate)
+    
+    //? Store
+
+    //? Update
+
+    //? Delete
 }
